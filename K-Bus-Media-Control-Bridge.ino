71906a5454761927fc6a00b2f5991e4c0a8ef4ab
Michael Dornisch <eovnu87435ds@mac.com>
2017-06-23 14:35:08 -0400

    HID functions added

---
+++ K-Bus-Media-Control-Bridge.ino
@@ -0,0 +1,33 @@
+#include "HID-Project.h"  //HID Project Library from NicoHood. Add via Library Manager
+const int runbutton = 6;
+
+void setup() {
+  pinMode(runbutton, INPUT_PULLUP);
+  while (!digitalRead(runbutton)) {
+    //This keeps the rest of the program running. useful for programming the atmega32u4
+  }
+  Keyboard.begin();
+}
+
+void loop() {
+
+}
+
+
+
+// HID functions for media control
+void launchGoogle(){
+  Keyboard.write(KeyboardKeycode(KEY_LEFT_GUI));
+}
+
+void sendPlayPause(){
+  Keyboard.write(KeyboardKeycode(MEDIA_PLAY_PAUSE));
+}
+
+void sendFastForward(){
+  Keyboard.write(KeyboardKeycode(MEDIA_FAST_FORWARD));
+}
+
+void sendRewind(){
+  Keyboard.write(KeyboardKeycode(MEDIA_REWIND));
+}
